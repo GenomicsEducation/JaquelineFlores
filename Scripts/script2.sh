1d0cadcb9caea5feee6f94d6c7953186d7b4fde9
Jaqs8 <80992964+Jaqs8@users.noreply.github.com>
2021-06-05 15:08:09 -0500

    Create script2.sh

---
+++ Scripts/script2.sh
@@ -0,0 +1,6 @@
+nano script2.sh
+
+# !/bin/bash
+# Descarga y descomprime SRA Toolkit
+wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz
+tar -xzf sratoolkit.current-centos_linux64.tar.gz
